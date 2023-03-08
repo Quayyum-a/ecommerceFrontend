c50aec8fbc4426922f353304141dd870838b5a6a
Majek11 <majeklekan9@gmail.com>
2023-03-07 18:14:36 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,49 @@
+import {
+  createBrowserRouter, RouterProvider, Outlet
+} from 'react-router-dom';
+import Footer from './components/Footer/Footer';
+import NavBar from './components/Navbar/NavBar';
+import Home from './pages/Home/Home';
+import Product from './pages/Product/Product';
+import Products from './pages/Products/Products';
+import './App.scss'
+
+const Layout = () => {
+  return (
+    <div className="app">
+      <NavBar />
+      <Outlet />
+      <Footer />
+    </div>
+  )
+}
+
+
+const router = createBrowserRouter ([
+  {
+    path:"/",
+    element:<Layout />,
+    children:[
+      {
+        path:"/",
+        element:<Home />
+      },
+      {
+        path:"/products/:id",
+        element:<Products />
+      },
+      {
+        path:"/product/:id",
+        element:<Product />
+      },
+    ]
+  },
+])
+
 function App() {
   return (
     <div>
-      Hello world!
+      <RouterProvider router={router} />
     </div>
   );
 }
