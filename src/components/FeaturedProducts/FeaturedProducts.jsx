45b9a262962e978e2bde03d4062583023c073638
Quayyum <quayyum@example.com>
2025-06-23 23:48:32 +0100

    feat: update featured products with diverse product categories and modern descriptions

--- src/components/FeaturedProducts/FeaturedProducts.jsx
+++ src/components/FeaturedProducts/FeaturedProducts.jsx
@@ -1,58 +1,61 @@
-import React from 'react'
-import Card from '../Card/Card'
-import './FeaturedProducts.scss'
+import React from "react";
+import Card from "../Card/Card";
+import "./FeaturedProducts.scss";
 
-const FeaturedProducts = ({type}) => {
-
-    const data = [
-        {
-            id: 1,
-            img: "https://images.pexels.com/photos/12753393/pexels-photo-12753393.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
-            img2: "https://images.pexels.com/photos/10040967/pexels-photo-10040967.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
-            title: "Long Sleeve Graphic T-shirt",
-            isNew:true,
-            oldPrice: 19,
-            price:12,
-        },
-        {
-            id: 2,
-            img: "https://images.pexels.com/photos/12753393/pexels-photo-12753393.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
-            img2: "https://images.pexels.com/photos/10040967/pexels-photo-10040967.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
-            title: "Long Sleeve Graphic T-shirt",
-            isNew:true,
-            oldPrice: 19,
-            price:12,
-        },
-        {
-            id: 3,
-            img: "https://images.pexels.com/photos/12753393/pexels-photo-12753393.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
-            img2: "https://images.pexels.com/photos/10040967/pexels-photo-10040967.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
-            title: "Long Sleeve Graphic T-shirt",
-            oldPrice: 19,
-            price:12,
-        },
-        {
-            id: 4,
-            img: "https://images.pexels.com/photos/12753393/pexels-photo-12753393.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
-            img2: "https://images.pexels.com/photos/10040967/pexels-photo-10040967.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
-            title: "Long Sleeve Graphic T-shirt",
-            oldPrice: 19,
-            price:12,
-        },
-    ]
+const FeaturedProducts = ({ type }) => {
+  const data = [
+    {
+      id: 1,
+      img: "https://images.pexels.com/photos/1647976/pexels-photo-1647976.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+      img2: "https://images.pexels.com/photos/1647976/pexels-photo-1647976.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+      title: "Premium Wireless Headphones",
+      isNew: true,
+      oldPrice: 299,
+      price: 199,
+    },
+    {
+      id: 2,
+      img: "https://images.pexels.com/photos/12753393/pexels-photo-12753393.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+      img2: "https://images.pexels.com/photos/10040967/pexels-photo-10040967.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+      title: "Designer Casual T-Shirt",
+      isNew: true,
+      oldPrice: 89,
+      price: 59,
+    },
+    {
+      id: 3,
+      img: "https://images.pexels.com/photos/1571460/pexels-photo-1571460.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+      img2: "https://images.pexels.com/photos/1571460/pexels-photo-1571460.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+      title: "Smart Home Assistant",
+      oldPrice: 199,
+      price: 149,
+    },
+    {
+      id: 4,
+      img: "https://images.pexels.com/photos/1571461/pexels-photo-1571461.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+      img2: "https://images.pexels.com/photos/1571461/pexels-photo-1571461.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+      title: "Modern Coffee Maker",
+      oldPrice: 159,
+      price: 119,
+    },
+  ];
   return (
-    <div className='featuredProducts'>
-        <div className="top">
-            <h1>{type} products</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora, dolores deserunt a vitae provident totam quos aperiam magni eaque aut? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias numquam reiciendis harum quis officia consequuntur pariatur repudiandae doloremque eligendi nulla.</p>
-        </div>
-        <div className="bottom">
-            {data.map(item=>(
-                <Card item={item} key={item.id} />
-            ))}
-        </div>
+    <div className="featuredProducts">
+      <div className="top">
+        <h1>{type} products</h1>
+        <p>
+          Discover amazing products across all categories. From cutting-edge
+          electronics to stylish fashion and everything in between. Quality
+          guaranteed with exceptional customer service.
+        </p>
+      </div>
+      <div className="bottom">
+        {data.map((item) => (
+          <Card item={item} key={item.id} />
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default FeaturedProducts
+export default FeaturedProducts;
